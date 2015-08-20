6040a83ced40c3a875e774c23a13ea44e09bd2e6
Ari Porad <ari@porad.com>
2015-08-20 07:45:15 -0700

    test(sort): test for handling of out-of-order commits

--- test/specs/index.js
+++ test/specs/index.js
@@ -1,7 +1,7 @@
 const test = require('tap').test
 const proxyquire = require('proxyquire')
 
-const tags = ['firstVersion',
+let tags = ['firstVersion',
               'thing',
               'v1.0',
               'foo',
@@ -26,7 +26,7 @@ const lastRelease = proxyquire('../../dist', {
 })
 
 test('last release from git tags', (t) => {
-  t.plan(2)
+  t.plan(3)
 
   t.test('last release from git tags', (tt) => {
     lastRelease({}, {}, (err, release) => {
@@ -39,8 +39,7 @@ test('last release from git tags', (t) => {
   })
 
   t.test('ignore invalid tags', (tt) => {
-    tags.splice(0, tags.length)
-    tags.push('foo', 'bar', 'baz', '1.0.0', 'v1.0', '1.0', 'v1.a.0')
+    tags = ['foo', 'bar', 'baz', '1.0.0', 'v1.0', '1.0', 'v1.a.0']
     lastRelease({}, {}, (err, release) => {
       tt.error(err)
       tt.same(release, {}, 'inital release')
@@ -49,5 +48,40 @@ test('last release from git tags', (t) => {
     })
   })
 
+  t.test('deal with unsorted versions', (tt) => {
+    tags = shuffle(['v1.0.0', 'v1.1.0', 'v1.1.1', 'v2.8.7', 'v3.6.9', 'v9.9.9'])
+
+    lastRelease({}, {}, (err, release) => {
+      tt.error(err)
+      tt.is(release.version, '9.9.9', 'version')
+      tt.is(release.gitHead, 'v9.9.9', 'gitHead')
+
+      tt.end()
+    })
+  })
+
   t.end()
 })
+
+// http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle (array) {
+  var currentIndex = array.length
+  var temporaryValue
+  var randomIndex
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+
+  return array
+}
+
